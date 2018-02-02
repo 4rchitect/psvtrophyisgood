19b29441398de0f32cd1dd3130c980082fd1da0f
Silica <earsyum@gmail.com>
2018-02-02 13:50:46 +1300

    Add files via upload

--- ParseTRPTRNS.py
+++ ParseTRPTRNS.py
@@ -1,27 +1,26 @@
-import os
+import datetime
 import binascii
 
-trpFile = "C:\Users\Matthew\Documents\PSVTROPHYISGOOD\data\decrypted\TRPTRANS.DAT"
+trpFile = "C:\Users\Matthew\Documents\PSVTROPHYISGOOD\data\DecryptedData\TRPTRANS.DAT"
 
-trpHandle = open(trpFile, "rb")
+trpData = open(trpFile, "rb").read()
 
 
 def getAccountId():
-    return binascii.hexlify(trpHandle.read()[0x120:0x120+0x8])
+    return binascii.hexlify(trpData[0x120:0x120+0x8])
 
 def makeCmaAid(aid):
     cmaAid = [aid[i:i + 2] for i in range(0, len(aid), 2)]
     cmaAid.reverse()
     return str(cmaAid)
 def getNumberOfUnlockedTrophies():
-    n = str(binascii.hexlify(trpHandle.read()[0x187:0x187+0x1]))
-    return int(n,16)
+    return int(str(binascii.hexlify(trpData[0x187:0x187+0x1])),16)
 
 def getNpCommId():
-    return trpHandle.read()[0x170:0x170 + 0x0C]
+    return trpData[0x170:0x170 + 0x0C]
 
 def getNpCommSign():
-    return binascii.hexlify(trpHandle.read()[0x19C:0x19C + 0x94])
+    return binascii.hexlify(trpData[0x190:0x190 + 0xAC])
 
 def findDataZone(v):
         begin = 0x2B7
@@ -36,9 +35,13 @@ def findDataZone(v):
 def getTrophyDataBlock(v):
     begin = findDataZone(v)["begin"]
     end = findDataZone(v)["end"]
-    return binascii.hexlify(trpHandle.read()[begin:end])
+    return binascii.hexlify(trpData[begin:end])
 
-a = 0
-num = getNumberOfUnlockedTrophies()
-print getTrophyDataBlock(0)
-a += 1
+def decodeTimestamp(timestamp):
+    timestamp = int(timestamp,16)
+    return datetime.datetime.fromordinal(1) + datetime.timedelta(microseconds=timestamp)
+
+def encodeTimestamp(dateandtime):
+    dt = datetime.datetime.strptime(dateandtime, "%Y-%m-%d %H:%M:%S.%f")
+    timestamp = (dt - datetime.datetime(1, 1, 1)).total_seconds() * 1000000
+    return hex(int(timestamp))[2:-1]
